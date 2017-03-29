6ecd73c843481879704a6e617a7257b8a8521021
Victor Shao <vshao98@gmail.com>
2017-03-29 12:38:43 -0400

    Redesign

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-function updateMainDisplay(clickedElement) {
-	var innerImg = clickedElement.getElementsByTagName('img')[0];
-	var mainDisplayImg = document.getElementById('mainDisplay').getElementsByTagName('img')[0];
-	mainDisplayImg.src = innerImg.src;
-}
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var folder = 'img/'
+
+images.forEach(function(image) {
+	var div = document.createElement('div');
+	div.style['background-image'] = 'url("' + folder + image + '")';
+	document.body.appendChild(div);
+});
