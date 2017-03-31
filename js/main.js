1bf48012d99e20da3c44c698422c6a6a814eaade
Victor Shao <vshao98@gmail.com>
2017-03-31 19:39:06 -0400

    Transitions reworked; hover in progress

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',
+			  '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
 var folder = 'img/'
 
-images.forEach(function(image) {
-	var div = document.createElement('div');
-	div.style['background-image'] = 'url("' + folder + image + '")';
-	document.body.appendChild(div);
-});
+loadImages();
+showImages();
+
+function loadImages() {
+	var footer = document.getElementById('footer');
+	images.forEach(function(image) {
+		var div = document.createElement('div');
+		div.className = 'picture';
+		div.style['background-image'] = 'url("' + folder + image + '")';
+		document.body.insertBefore(div, footer);
+	});
+}
+
+function showImages() {
+	var imageList = document.getElementsByClassName('picture');
+	window.getComputedStyle(imageList[0]).opacity;
+	showImage(imageList, 0);
+}
+
+function showImage(imageList, imageNum) {
+	imageList[imageNum].style.opacity = 1;
+	if (imageNum + 1 < imageList.length) {
+		setTimeout(function() {
+			showImage(imageList, imageNum + 1);
+		}, 100);
+	}
+}
